63d8ef80ec23cc00d7aa5bea4edc85d446ca2fe8
Justin Bay <justin.bay@outlook.com>
2016-06-23 19:03:14 -0400

    add "no-property-initializers"

---
+++ rules/noPropertyInitializersRule.ts
@@ -0,0 +1,34 @@
+import * as Lint from 'tslint/lib/lint';
+import * as ts from 'typescript';
+
+export class Rule extends Lint.Rules.AbstractRule {
+	public apply(sourceFile: ts.SourceFile) {
+		return this.applyWithWalker(new ClassMethodNewlinesWalker(sourceFile, this.getOptions()));
+	}
+}
+
+class ClassMethodNewlinesWalker extends Lint.RuleWalker {
+	public visitClassDeclaration(node: ts.ClassDeclaration) {
+		super.visitClassDeclaration(node);
+		this.validate(node);
+	}
+
+	public visitClassExpression(node: ts.ClassExpression) {
+		super.visitClassExpression(node);
+		this.validate(node);
+	}
+
+	private validate(node: ts.ClassLikeDeclaration) {
+		for (const member of node.members) {
+			if ((member as ts.PropertyDeclaration).initializer) {
+				this.addFailure(
+					this.createFailure(
+						member.name.getStart(),
+						member.name.getWidth(),
+						'property initializers are nonstandard -- assign in constructor or method'
+					)
+				);
+			}
+		}
+	}
+}
