21c5397f0fd348a0f120bc48f93850323832b5bf
Justin Bay <justin.bay@outlook.com>
2016-06-23 19:48:19 -0400

    add "camel-case-local-functions"

---
+++ rules/camelCaseLocalFunctionsRule.ts
@@ -0,0 +1,32 @@
+import * as Lint from 'tslint/lib/lint';
+import * as ts from 'typescript';
+
+export class Rule extends Lint.Rules.AbstractRule {
+	public apply(sourceFile: ts.SourceFile) {
+		return this.applyWithWalker(new CamelCaseFunctionsWalker(sourceFile, this.getOptions()));
+	}
+}
+
+class CamelCaseFunctionsWalker extends Lint.RuleWalker {
+	public visitCallExpression(node: ts.CallExpression) {
+		if (this.isIdentifier(node.expression)) {
+			const name = node.expression as ts.Identifier;
+			const firstLetter = name.text.charAt(0);
+			if (firstLetter !== firstLetter.toLowerCase()) {
+				this.addFailure(
+					this.createFailure(
+						name.getStart(this.getSourceFile()),
+						name.getWidth(this.getSourceFile()),
+						'local function names should be camelCase'
+					)
+				);
+			}
+		}
+
+		super.visitCallExpression(node);
+	}
+
+	private isIdentifier(node: ts.Node): node is ts.Identifier {
+		return node.kind === ts.SyntaxKind.Identifier;
+	}
+}
