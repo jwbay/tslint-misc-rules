478929def2fb76d38791708e620226d19c9b27fb
Justin Bay <justin.bay@outlook.com>
2016-07-03 13:34:54 -0400

    add "no-braces-for-single-line-lambda"

---
+++ rules/noBracesForSingleLineLambdaRule.ts
@@ -0,0 +1,34 @@
+import * as Lint from 'tslint/lib/lint';
+import * as ts from 'typescript';
+
+export class Rule extends Lint.Rules.AbstractRule {
+	public apply(sourceFile: ts.SourceFile) {
+		return this.applyWithWalker(new NoBracesForSingleLineLambdaWalker(sourceFile, this.getOptions()));
+	}
+}
+
+class NoBracesForSingleLineLambdaWalker extends Lint.RuleWalker {
+	protected visitArrowFunction(node: ts.ArrowFunction) {
+		super.visitArrowFunction(node);
+		const { body } = node;
+
+		if (this.functionBodyIsBraced(body) && this.functionBodyIsOneLine(body)) {
+			this.addFailure(
+				this.createFailure(
+					body.getStart(this.getSourceFile()),
+					1,
+					'single-line lambdas should not be wrapped in braces'
+				)
+			);
+		}
+	}
+
+	private functionBodyIsBraced(node: ts.ConciseBody): node is ts.Block {
+		return node && node.kind === ts.SyntaxKind.Block;
+	}
+
+	private functionBodyIsOneLine(node: ts.Block) {
+		const bodyText = node.getFullText(this.getSourceFile());
+		return !bodyText.match(/\n/);
+	}
+}
