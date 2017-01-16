43276bf513f4132425acc8a037e0024519e9cda0
Justin Bay <justin.bay@outlook.com>
2017-01-15 20:18:06 -0500

    fix various issues with single line arrow functions autofix
    fixes #14

--- rules/noBracesForSingleLineArrowFunctionsRule.ts
+++ rules/noBracesForSingleLineArrowFunctionsRule.ts
@@ -17,15 +17,15 @@ class NoBracesForSingleLineArrowFunctionsWalker extends Lint.RuleWalker {
 			this.functionBodyHasOneStatement(body) &&
 			this.functionBodyIsOneLine(body)
 		) {
-			const failureStart = body.getStart(this.getSourceFile());
-			const text = body.getChildAt(1).getText().replace('return', '').replace(';', '').trim();
+			const sf = this.getSourceFile();
+			const failureStart = body.getStart(sf);
 			const fix = new Lint.Fix('no-braces-for-single-line-arrow-functions', [
-				this.createReplacement(failureStart, body.getWidth(), text)
+				this.createReplacement(failureStart, body.getWidth(sf), this.getFixedText(body))
 			]);
 			this.addFailure(
 				this.createFailure(
 					failureStart,
-					body.getWidth(),
+					body.getWidth(sf),
 					'single-line arrow functions should not be wrapped in braces',
 					fix
 				)
@@ -44,5 +44,29 @@ class NoBracesForSingleLineArrowFunctionsWalker extends Lint.RuleWalker {
 	private functionBodyIsOneLine(node: ts.Block) {
 		const bodyText = node.getFullText(this.getSourceFile());
 		return !bodyText.match(/\n/);
+	}
+
+	private getFixedText(node: ts.Block) {
+		const body = node.getChildAt(1);
+		const statement = body.getChildAt(0);
+		let result = this.stripSemicolon(body.getText(this.getSourceFile()));
+		if (statement && statement.kind === ts.SyntaxKind.ReturnStatement) {
+			result = result.replace('return', '').trim();
+
+			const returnExpression = statement.getChildAt(1);
+			if (returnExpression && returnExpression.kind === ts.SyntaxKind.ObjectLiteralExpression) {
+				result = `(${result})`;
+			}
+		}
+
+		return result;
+	}
+
+	private stripSemicolon(text: string) {
+		let result = text.trim();
+		if (result.match(/;$/)) {
+			result = result.slice(0, text.length - 1);
+		}
+		return result;
 	}
 }
