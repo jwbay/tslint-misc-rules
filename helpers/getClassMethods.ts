863cb5969ad6d0e316c6e93ad5f6df4ac808bdb3
Justin Bay <justin.bay@outlook.com>
2017-01-16 14:07:59 -0500

    treat class method members and class arrow function members the same

--- helpers/getClassMethods.ts
+++ helpers/getClassMethods.ts
@@ -1,9 +1,20 @@
 import * as ts from 'typescript';
+import nodeIsKind from './nodeIsKind';
 
 export default function getClassMethods(node: ts.ClassLikeDeclaration) {
 	if (!node.members) {
 		return [];
 	}
 
-	return node.members.filter(m => m.kind === ts.SyntaxKind.MethodDeclaration) as ts.MethodDeclaration[];
+	return node.members.filter(m => {
+		if (nodeIsKind<ts.MethodDeclaration>(m, k => k.MethodDeclaration)) {
+			return true;
+		}
+
+		if (nodeIsKind<ts.PropertyDeclaration>(m, k => k.PropertyDeclaration)) {
+			return nodeIsKind(m.initializer, k => k.ArrowFunction);
+		}
+
+		return false;
+	}) as (ts.MethodDeclaration | ts.PropertyDeclaration)[];
 }
