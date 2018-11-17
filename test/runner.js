24d4e3b58fe41fcbf7ed19910c74e8d6464dfe75
Justin Bay <justin.bay@outlook.com>
2018-11-17 10:41:06 -0500

    prettier js files

--- test/runner.js
+++ test/runner.js
@@ -1,31 +1,35 @@
-var fs = require('fs');
-var path = require('path');
-var os = require('os');
-var execSync = require('child_process').execSync;
-var pattern = process.argv[2];
+var fs = require('fs')
+var path = require('path')
+var os = require('os')
+var execSync = require('child_process').execSync
+var pattern = process.argv[2]
 
-var directories = fs.readdirSync(path.join(process.cwd(), 'test'))
-    .filter(name => !path.extname(name));
+var directories = fs
+	.readdirSync(path.join(process.cwd(), 'test'))
+	.filter(name => !path.extname(name))
 
 if (pattern) {
-    directories = directories.filter(name => name.match(new RegExp(pattern)));
+	directories = directories.filter(name => name.match(new RegExp(pattern, 'i')))
 }
 
-var exitCode = 0;
+var exitCode = 0
 
 directories.forEach(testFolder => {
-    try {
-        execSync('node ./node_modules/tslint/lib/tslint-cli.js --test test/' + testFolder, {
-            stdio: 'inherit',
-            encoding: 'utf8'
-        });
-    } catch (err) {
-        if (typeof err.signal === 'undefined') {
-            console.error(err.stack);
-        }
-        exitCode = 1;
-        console.log(os.EOL, os.EOL);
-    }
-});
+	try {
+		execSync(
+			'node ./node_modules/tslint/lib/tslint-cli.js --test test/' + testFolder,
+			{
+				stdio: 'inherit',
+				encoding: 'utf8',
+			}
+		)
+	} catch (err) {
+		if (typeof err.signal === 'undefined') {
+			console.error(err.stack)
+		}
+		exitCode = 1
+		console.log(os.EOL, os.EOL)
+	}
+})
 
-process.exit(exitCode);
+process.exit(exitCode)
--- test/watch.js
+++ test/watch.js
@@ -1,28 +1,29 @@
-var chokidar = require('chokidar');
-var execSync = require('child_process').execSync;
-var path = require('path');
+var chokidar = require('chokidar')
+var execSync = require('child_process').execSync
+var path = require('path')
 
-chokidar.watch('rules/*.js', { ignoreInitial: true })
-    .on('change', file => handleSourceChange(file))
-    .on('add', file => handleSourceChange(file));
-chokidar.watch('test/**/*.{json,lint,fix}', { ignoreInitial: true })
-    .on('change', file => handleTestChange(file))
-    .on('add', file => handleTestChange(file));
+chokidar
+	.watch('rules/*.js', { ignoreInitial: true })
+	.on('change', file => handleSourceChange(file))
+	.on('add', file => handleSourceChange(file))
+chokidar
+	.watch('test/**/*.{json,lint,fix}', { ignoreInitial: true })
+	.on('change', file => handleTestChange(file))
+	.on('add', file => handleTestChange(file))
 
 function handleSourceChange(file) {
-    runTest(path.basename(file, path.extname(file)));
+	runTest(path.basename(file, path.extname(file)))
 }
 
 function handleTestChange(file) {
-    var dirs = path.dirname(file).split(path.sep);
-    runTest(dirs[dirs.length - 1])
+	var dirs = path.dirname(file).split(path.sep)
+	runTest(dirs[dirs.length - 1])
 }
 
 function runTest(ruleName) {
-    try {
-        execSync('node test/runner ' + ruleName, {
-            stdio: 'inherit'
-        });
-    } catch (e) {
-    }
+	try {
+		execSync('node test/runner ' + ruleName, {
+			stdio: 'inherit',
+		})
+	} catch (e) {}
 }
